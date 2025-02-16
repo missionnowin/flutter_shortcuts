6ea0efe2139c2331fdd9dc4922e720312b2b3a59
Krille <c.kussowski@famedly.com>
2025-02-16 11:06:53 +0100

    refactor: Rename to correct plugin

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shortcuts_new/flutter_shortcuts.dart';
+import 'package:flutter_shortcuts_new/flutter_shortcuts_new.dart';
 
 void main() {
   runApp(MyApp());
