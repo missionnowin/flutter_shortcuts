731ee72f2f2ae8d20a86b81bc29cc879f167e30c
Mikhail Zharov <mike.zharov91@gmail.com>
2025-09-15 18:54:48 +0300

    Update MethodCallImplementation.java

--- android/src/main/java/im/fluffychat/flutter_shortcuts_new/MethodCallImplementation.java
+++ android/src/main/java/im/fluffychat/flutter_shortcuts_new/MethodCallImplementation.java
@@ -12,6 +12,13 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.Icon;
 import android.os.Build;
 import android.util.Log;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Bitmap.Config;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
